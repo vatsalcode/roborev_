1401725edc59160c542ffdc3f935f751f1e96f54
Stuti <stuti.2996@gmail.com>
2023-12-16 22:37:39 -0600

    updated scheduling logic

--- kafka_producer.py
+++ kafka_producer.py
@@ -6,18 +6,24 @@ config_parser = ConfigParser()
 config_parser.read('config.ini')
 config = dict(config_parser['default'])
 
-def sendToKafka(topic,keyId,info):
+def produce_kafka_message(topic,keyId,info):
     producer = Producer(config)
-    producer.produce(topic, key=json.dumps(keyId), value=json.dumps(info))
+    producer.produce(topic, key=str(keyId), value=json.dumps(info))
     producer.flush()
 
-keyId = {"task_id": 4}
-info = {
-  "task_id": 4,
-  "taskname": "collect",
-  "start_timestamp": 1702641600000000,
-  "end_timestamp": 1702679400000000,
-  "current_status": "In Progress",
-  "robot_id": 3
-}
-sendToKafka("roborev.tracking.taskworkinginfo",keyId,info)
+# keyId = {"task_id": 4}
+# info = {
+#   "task_id": 4,
+#   "taskname": "collect",
+#   "start_timestamp": 1702641600000000,
+#   "end_timestamp": 1702679400000000,
+#   "current_status": "In Progress",
+#   "robot_id": 3
+# }
+
+# robot_info = {
+#   "robot_id": "1",
+#   "robot_name": "Frank",
+#   "status": "Available"
+# }
+# produce_kafka_message("update_robot",robot_info['robot_id'],robot_info)
